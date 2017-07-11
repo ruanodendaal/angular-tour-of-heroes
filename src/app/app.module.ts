853b08034bf6f99939dbf56969cbc5ed204e59cc
Ruan Odendaal <ruanodendaal@gmail.com>
2017-07-11 18:26:41 +0100

    separate concerns of hero detail and hero itself

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 
 import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule,
+    FormsModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent
   ],
-  bootstrap: [
-    AppComponent
-  ]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
