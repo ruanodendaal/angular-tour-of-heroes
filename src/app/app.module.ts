2b5818a4f068d5fcd56d18982a386c401828c0cf
Ruan Odendaal <ruanodendaal@gmail.com>
2017-07-12 12:14:55 +0100

    refactor to individual routes and seperate css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,30 +8,13 @@ import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
 import { HeroService }         from './hero.service';
 import { DashboardComponent }  from './dashboard.component';
+import { AppRoutingModule }    from './app-routing.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: 'detail/:id',
-        component: HeroDetailComponent
-      },
-    ])
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
@@ -39,9 +22,7 @@ import { DashboardComponent }  from './dashboard.component';
     HeroDetailComponent,
     HeroesComponent
   ],
-  providers: [
-    HeroService
-  ],
+  providers: [ HeroService ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
